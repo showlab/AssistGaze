e0ec48cb74fc2dadccdcdfd8256162509c9a591c
Chenanism777 <75237329+Chenanism777@users.noreply.github.com>
2025-11-10 04:19:45 +0800

    Delete gazevqa directory

--- gazevqa/pipelines/encoders/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Shared encoder utilities used across the gazevqa pipelines package.
-"""
-
-from .text import TextEncoder
-from .video import VideoClipEncoder
-from .image import RoiFeatureExtractor
-
-__all__ = ["TextEncoder", "VideoClipEncoder", "RoiFeatureExtractor"]
